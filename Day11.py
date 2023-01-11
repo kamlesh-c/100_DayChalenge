671241ff411da8b02c3fe9f3d7acecc5385ce1c6
kamlesh-c <kamlesh.c89@gmail.com>
2023-01-11 18:06:59 +0530

    Day 11 String operation

---
+++ Day11.py
@@ -0,0 +1,28 @@
+#Day 11
+#String operation
+
+name = "Kamlesh"
+print("Name is =", name, "and type of name is",type(name))
+apple= "Hi This is my File, you can't edit \"this fle\""
+print (apple)
+# user ''' for multiline string
+address = '''11/33 
+"OM Niwas" 
+MG road City '''
+print (name[0])
+print (name[1])
+print (name[2])
+print (name[3])
+print (name[4])
+print (name[5])
+print (name[6])
+print ("******")
+print(name[0:4])
+print(name[4:])
+print ("******")
+print(address)
+print ("******")
+print ("All charector of Address ")
+print ("******")
+for charator in address:
+    print(charator)
