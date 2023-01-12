078be8375720a7182ac06fada19ebd14b5c3acb9
kamlesh-c <kamlesh.c89@gmail.com>
2023-01-12 21:52:39 +0530

    Day 12 String Slicing

---
+++ Day12.py
@@ -0,0 +1,14 @@
+#Day 12
+#String slicing
+
+name = "Kamlesh"
+print (name)
+print("Length of Name is ",len(name))
+print("first 3 letter of name =",name[0:3])
+print("print all letter=",name[:])
+print("Negtive slicing last letter",name[-1])
+print("Negative slicing",name[-3:0])
+for charetor in name:
+    print(charetor)
+a= "Harry"
+print (a[-4:-2])
