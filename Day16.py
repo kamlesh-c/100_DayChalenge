0da68ce5dde5fc3f6d8b58fedbb21082f7e6737f
kamlesh-c <kamlesh.c89@gmail.com>
2023-01-17 19:55:39 +0530

    Day 16 Match statement

--- Day16.py
+++ Day16.py
@@ -1,5 +1,15 @@
 #Day 16
 #Match case
 #this new update from 3.10
-x= int(input("Enter a number: "))
-
+# x= int(input("Enter a number: "))
+x = 3
+match x:
+    case 0:
+        print("Case is zero")
+    case 5:
+        print("Case is 5")
+    case 4:
+        print("Case is 4")
+    case _:
+        print("this is default x")
+        #this is default case define by _
