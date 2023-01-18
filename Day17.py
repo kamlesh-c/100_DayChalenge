b66cdcf8115be2660d04b38879052f02b57b70bb
kamlesh-c <kamlesh.c89@gmail.com>
2023-01-18 19:07:39 +0530

    Day 18 while loop

--- Day17.py
+++ Day17.py
@@ -1,5 +1,7 @@
 # Day 17
 # For loop
 x = int(1)
-for i in range(0,10,2):
-    print("Number: ", i)
+for i in range(0,10):
+    print (i)
+
+print
---
+++ Day18.py
@@ -0,0 +1,8 @@
+# Day 18
+# while loops
+i = int(0)
+while (i <=10):
+    print (i)
+    i=i+1
+else:
+    print(i,"in elase")
