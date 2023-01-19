f525f4ab131db6ac92e237acc685e92fee9484ae
kamlesh-c <kamlesh.c89@gmail.com>
2023-01-19 18:35:58 +0530

    Day 18 while loop

---
+++ Day19.py
@@ -0,0 +1,17 @@
+# day 19
+# Break and continue statement
+for i in range(5):
+    print ("If i = 8  Break ")
+    print ("Print is =", i*2)
+    if (i == 8):
+        break
+
+print("out of loop")
+
+for i in range (10):
+    print("This is i", i)
+    if (i == 5 ):
+        print("in if ")
+        continue
+    print("after if statement")
+print("after for loop")
