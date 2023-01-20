a6d85d0eeadee1a83116fb6473050c254863d316
kamlesh-c <kamlesh.c89@gmail.com>
2023-01-20 19:45:31 +0530

    Day 20  Do wile loop emulation

---
+++ Day19_2.py
@@ -0,0 +1,11 @@
+#day 20
+#doning same assignment for day for function
+#do while  loops
+i = int(1)
+while True:
+     print (i)
+     i = i+1
+     if (i == 7):
+         print("In if before break")
+         break
+print("Out of loop")
