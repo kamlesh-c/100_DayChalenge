e2d893677ea866329cb418bf7815cbea1f90e6ae
kamlesh-c <kamlesh.c89@gmail.com>
2023-01-21 18:30:49 +0530

    Update code use funtion for claculation

--- Self_Exercies_FD_Calculator.py
+++ Self_Exercies_FD_Calculator.py
@@ -11,31 +11,35 @@ print ("For 46 days to 90 days 4.25%")
 print("For 91 days to 180 days 5%")
 print("For 181 days to 365 days 6.10%")
 
+print ("************")
+
 priciple = int(input("please enter Principle ampunt: "))
-tenur= int(input("Please select Tenure for FD"))
+tenur= int(input("Please select Tenure for FD: "))
+
+print ("************")
+
+def calculateamt(t,i):
+    interest_amt = (priciple * t * i / 100)
+    print ("your Interest rate is: ", i)
+    print("Interest amount:", ('%.2f' % interest_amt))
+    print("Total amount:", ('%.2f' % (priciple + interest_amt)))
+
 if tenur >=7 and tenur <=45:
     t= tenur/365
     i= 3.75
-    interest_amt= (priciple * t * i /100)
-    print ("Interest amount:", ('%.2f' % interest_amt))
-    print("Total amount:", ('%.2f' % (priciple+interest_amt)))
+    calculateamt(t,i)
+
 elif tenur >=46 and tenur <=90:
     t= tenur/365
     i= 4.25
-    interest_amt= (priciple * t * i /100)
-    print ("Interest amount:", ('%.2f' % interest_amt))
-    print("Total amount:", ('%.2f' % (priciple+interest_amt)))
+    calculateamt(t,i)
 elif tenur >=91 and tenur <=180:
     t= tenur/365
     i= 5
-    interest_amt= (priciple * t * i /100)
-    print ("Interest amount:", ('%.2f' % interest_amt))
-    print("Total amount:", ('%.2f' % (priciple+interest_amt)))
+    calculateamt(t,i)
 elif tenur >=181 and tenur <=365:
     t= tenur/365
     i= 6.15
-    interest_amt= (priciple * t * i /100)
-    print ("Interest amount:", ('%.2f' % interest_amt))
-    print("Total amount:", ('%.2f' % (priciple+interest_amt)))
+    calculateamt(t,i)
 else:
     print("Invalid choice")
