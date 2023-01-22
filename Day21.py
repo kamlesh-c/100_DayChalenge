74b1136e856759bea2ac64aa14b934d3893d6e11
kamlesh-c <kamlesh.c89@gmail.com>
2023-01-22 19:11:23 +0530

    Day 21 Not completed will add monday

---
+++ Day21.py
@@ -0,0 +1,14 @@
+#Day 21
+#funtion arguemtn
+# tyep of funtion arguments
+
+print ("1) Required argument")
+print ("in Bellow funtion a and b are required argment")
+a= 1
+b = 2
+def average(a,b):
+    print ("a=1 and b= 2 averrage=",(a+b)/2)
+
+average(a,b)
+
+
